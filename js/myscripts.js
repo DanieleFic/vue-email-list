573cafb590e2eedf47a918a9604f0d419f2fbf4c
DanieleFic <reb_ficus22@hotmail.it>
2022-01-25 15:02:58 +0100

    first commit

---
+++ js/myscripts.js
@@ -0,0 +1,77 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+    email: []
+    },
+
+    methods: {
+        cicloEmail: function (){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (risposta) => {
+            // handle success
+            this.email.push(risposta.data.response)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+        },
+
+
+        creaEmail: function(){
+            for ( i = 0; i  < 10; i++) {
+                this.cicloEmail()
+            }
+        }
+}})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*mounted:{ function (){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (risposta) => {
+            // handle success
+            this.email.push = risposta.data.response
+            console.log(this.email)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+        
+        },*/
+            
+    
